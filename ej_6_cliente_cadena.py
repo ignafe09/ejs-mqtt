2ed83c487f1257bba223f187dc9a9d489bfb4e98
Ignacio Fernandez Sanchez-Pascuala <123626884+ignafe09@users.noreply.github.com>
2023-04-21 10:05:12 +0200

    Update ej_6_cliente_cadena.py

--- ej_6_cliente_cadena.py
+++ ej_6_cliente_cadena.py
@@ -5,10 +5,11 @@ from paho.mqtt.client import Client
 import sys
 from time import sleep
 
+#Esto es un ejemplo de cadena de clientes, pero se pueden hacer mas y de otras formas.
 values = []
 
 # Función para determinar si un número es primo
-def is_prime(n):
+def es_primo(n):
     if n < 2:
         return False
     for i in range(2, int(n**0.5) + 1):
@@ -27,10 +28,11 @@ def on_connect(client, rc):
     client.subscribe("numbers")
     
 # Función que se ejecutará cuando se reciba un mensaje en el topic "numbers"
+# Lee el numero, si es primo, se pone a escuchar humedad 5 seg.
 def on_message_numbers(client, userdata, message):
     number = int(message.payload.decode())
     print(f"Número recibido: {number}")
-    if is_prime(number):
+    if es_primo(number):
         print("Número primo, leyendo humedad durante 5 segundos...")
         values.clear()
         client.subscribe("humidity")
@@ -38,14 +40,17 @@ def on_message_numbers(client, userdata, message):
         client.unsubscribe("humidity")
 
 # Función que se ejecutará cuando se reciba un mensaje en el topic "humidity"
+# Mientras escucha humedad, va leyendo mensajes y los añade a una lista,
+#cuando llega a recibir 5 mensajes, calcula media y devuelve por pantalla,
+#y vacia la lista. Si la media supera cierto valor, se pone un temporizador.
+
 def on_message_humidity(client, userdata, message):
     humidity = float(message.payload.decode())
     print(f"Received humidity: {humidity}")
     values.append(humidity)
     if len(values) >= 5:
         mean = calculate_mean(values)
-        print(f"Media humedad en 5 segundos: {mean}")
-        values.clear()
+        print(f"Media humedad: {mean}")
     if mean > 20:
         print('Humdedad umbral superada, poniendo temporizador...')
         client.publish('temporizador', 'humedad_umbral;5;superada')
